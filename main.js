02b5ba48179b5aa7646d3d2654feae56a8c5d0f7
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2025-01-15 18:00:54 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+if(parseInt(process.version.replace("v","")) < 16) {
+   console.warn("\x1b[31mWarning!\x1b[0m\n\x1b[33ozo.js require node js version 16+ or  higher version to be work, your current node js version:", process.version, "please update your node js version and try again\x1b[0m")
+   process.exit()
+} else {
+   const client = require("./src/index.js")
+   require("./src/handler/string.js")
+
+module.exports = {
+    Bot: client.Bot,
+    CommandHandler: client.CommandHandler
+   }
+}
