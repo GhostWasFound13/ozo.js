ac65d0bbd9b08ad0c7978861a2b93ef17b2ed95d
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2025-01-15 18:09:26 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-if(parseInt(process.version.replace("v","")) < 16) {
-   console.warn("\x1b[31mWarning!\x1b[0m\n\x1b[33ozo.js require node js version 16+ or  higher version to be work, your current node js version:", process.version, "please update your node js version and try again\x1b[0m")
-   process.exit()
+if (parseInt(process.version.replace("v", "")) < 16) {
+    console.warn(
+        "\x1b[31mWarning!\x1b[0m\n\x1b[33mOzo.js requires Node.js version 16 or higher to work. Your current Node.js version:",
+        process.version,
+        "Please update your Node.js version and try again.\x1b[0m"
+    );
+    process.exit();
 } else {
-   const client = require("./src/index.js")
-   require("./src/handler/string.js")
+    const client = require("./src/index.js"); // Import the Bot class
+    const { CommandHandler } = require("./src/core/CommandHandler.js"); // Import the CommandHandler class
+    require("./src/handler/string.js"); // Import additional handlers or utilities
 
-module.exports = {
-    Bot: client.Bot,
-    CommandHandler: client.CommandHandler
-   }
+    module.exports = {
+        Bot: client.Bot,
+        CommandHandler, // Export CommandHandler directly
+    };
 }
