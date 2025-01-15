bbeb5cd80db047a7b0346e7f46d41ee60a47e2f2
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2025-01-15 18:06:12 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -223,36 +223,6 @@ class Bot {
     }
 }
 
-class CommandHandler {
-    constructor(opts) {
-        this.bot = opts.client || opts.bot;
-    }
-
-    load(folder) {
-        const bot = this.bot;
-        const consoleText = [];
-        const dirFolder = path.join(process.cwd(), folder);
-
-        const files = fs.readdirSync(dirFolder).filter((file) => file.endsWith("js"));
-        files.forEach((x) => {
-            try {
-                const theFile = require(`${dirFolder}/${x}`);
-                const theCmd = bot.cmd[theFile?.type || "default"];
-                if (theCmd !== undefined) {
-                    theCmd.set(theFile.name, theFile);
-                    consoleText.push("Loaded " + dirFolder + "/" + x);
-                } else {
-                    consoleText.push("Command type is invalid " + dirFolder + "/" + x);
-                }
-            } catch (e) {
-                consoleText.push("Failed to load " + dirFolder + "/" + x);
-            }
-        });
-        console.log(consoleText.join("\n|-------------------------------|\n"));
-    }
-}
-
 module.exports = {
     Bot,
-    CommandHandler,
 };
