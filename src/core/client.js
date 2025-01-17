509a3d1e60f0b39a1d241b6b2dcb4a9e73ad3e2a
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2025-01-17 17:38:59 +0700

    Create client.js

---
+++ src/core/client.js
@@ -0,0 +1,64 @@
+const { Bot } = require("./base.js");
+const { Client } = require("discord.js");
+const api = require("./handler/api.js");
+const version = require("../package.json").version;
+
+class ClientBot extends Bot {
+    constructor(opt) {
+        super(opt);
+        this.client = new Client({
+            intents: this.opt.intents.map((intent) => GatewayIntentBits[intent]),
+            partials: this.opt.partials.map((partial) => Partials[partial]),
+        });
+        this.client.simpler = this;
+        this.#start();
+    }
+
+    #start() {
+        // Load events dynamically
+        this.events.forEach((event) => {
+            if (typeof this[`on${event}`] === "function") {
+                this[`on${event}`]();
+            }
+        });
+
+        // Shard events
+        if (this.events.includes("ShardReady")) this.onShardReady();
+        if (this.events.includes("ShardDisconnect")) this.onShardDisconnect();
+        if (this.events.includes("ShardReconnecting")) this.onShardReconnecting();
+    }
+
+    onShardReady() {
+        this.client.on("shardReady", async (id, guilds) => {
+            await require("./handler/command/shardReady.js")(id, this);
+        });
+    }
+
+    onShardDisconnect() {
+        this.client.on("shardDisconnect", async (event, id) => {
+            await require("./handler/command/shardDisconnect.js")(event, id, this);
+        });
+    }
+
+    onShardReconnecting() {
+        this.client.on("shardReconnecting", async (id) => {
+            await require("./handler/command/shardReconnecting.js")(id, this);
+        });
+    }
+
+    onMessage() {
+        this.client.on("messageCreate", async (msg) => {
+            await require("./handler/command/default.js")(msg, this);
+            await require("./handler/command/always.js")(msg, this);
+        });
+    }
+
+    async login(token) {
+        await this.client.login(token);
+        this.client.prefix = this.prefix;
+        console.log(`Initialized on ${this.client.user.tag}\nMade with: discord.js v14\nv${version}\nJoin official support server: https://discord.gg/DW4CCH236j`);
+        api(this);
+    }
+}
+
+module.exports = ClientBot;
